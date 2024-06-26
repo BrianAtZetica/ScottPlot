b49c67cca1a1870c6b6381bd40c69238a873c077
BrianAtZetica <brian@zetica.com>
2024-06-26 11:39:22 +0930

    console sandbox changes to test 1D heatmap smoothing

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
@@ -1,8 +1,19 @@
 ﻿using ScottPlot;
+using System.Reflection.Emit;
+using static System.Net.Mime.MediaTypeNames;
 
 Plot plt = new();
-plt.Add.Signal(Generate.Sin());
-plt.Add.Signal(Generate.Cos());
+//plt.Add.Signal(Generate.Sin());
+//plt.Add.Signal(Generate.Cos());
+double[,] data = {
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 },
+}; 
+
+var hm = plt.Add.Heatmap(data);
+
+hm.VerticalSmooth = true;
 
 plt.SavePng("test.png", 600, 300).LaunchFile();
 plt.SavePng("test.png", 600, 300).LaunchInBrowser();
